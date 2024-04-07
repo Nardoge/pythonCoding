a660d9c1eadd1069730457ca30e09688ba04bd98
huangjunwei <nardoge@163.com>
2024-04-07 11:08:17 +0800

    names.py

---
+++ 3.1/names.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 10:44:50 2024
+
+@author: nardo
+"""
+names ={"joe","nardo","ray","hanmeimei"};
+
+for name in names:
+    print("我有一个朋友，名字叫:" + name);
+
