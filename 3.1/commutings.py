7625e56764fd355e93d1f29ef39c40d5e16284fc
huangjunwei <nardoge@163.com>
2024-04-07 13:51:55 +0800

    feat():3.1-3

---
+++ 3.1/commutings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 13:48:19 2024
+
+@author: nardo
+"""
+
+commutings = ["Honda","tailing","aima","xiaoniu"];
+
+for commuting in commutings:
+    print("I would like to own a "+ commuting+".")
---
+++ 3.1/namesHello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 10:44:50 2024
+
+@author: nardo
+"""
+names ={"joe","nardo","ray","hanmeimei"};
+
+for name in names:
+    print("新年快乐，" + name.title());
+
