06bbb2616556852e8b74c8e3a5d34eaf55107550
huangjunwei <nardoge@163.com>
2024-04-08 08:27:11 +0800

    feat():3.2

---
+++ 3.2/invite.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 08:22:50 2024
+
+@author: nardo
+"""
+
+inviteList = ["MaYun","LiHongYan","MaHuaTeng"];
+
+for invite in inviteList:
+    print("我邀请" + invite + "参加晚餐。")
