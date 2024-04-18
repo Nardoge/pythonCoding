10c07a307285b40df70991e4381252042d3ec06e
huangjunwei <nardoge@163.com>
2024-04-18 17:23:56 +0800

    fix(): 调整4.1

--- 4.1/pizza.py
+++ 4.1/pizza.py
@@ -8,3 +8,13 @@ pizzas = ["达美乐","尊宝","快速"]
 
 for pizza in pizzas:
     print(pizza);
+    
+
+for pizza in pizzas:
+    print("我最喜欢吃" + pizza +"!");
+    
+print("我们这里有的披萨店是:")
+for pizza in pizzas:
+    print(pizza);
+
+print("I really love pizza!");    
