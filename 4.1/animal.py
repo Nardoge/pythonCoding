702f30d25616b039eaad1e30c9a18b95493a963a
huangjunwei <nardoge@163.com>
2024-04-19 08:57:26 +0800

    feat():4.1,4.2

---
+++ 4.1/animal.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 08:51:02 2024
+
+@author: nardo
+"""
+
+animals =["猫","狗","鸟"];
+
+print("以下都可以作为宠物:");
+
+for animal in animals:
+    print(animal);
+
+
+for animal in animals:
+    print("人人都喜欢" + animal);
