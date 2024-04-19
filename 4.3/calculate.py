efc90bcff6b60e45666716000e7804cb9a4acf06
huangjunwei <nardoge@163.com>
2024-04-19 15:43:19 +0800

    feat():4.4 ,4.5

---
+++ 4.3/calculate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 15:29:35 2024
+
+@author: nardo
+"""
+
+nums = range(1,1000001);
+
+print(min(nums));
+
+print(max(nums));
+
+print(sum(nums));
+
+
---
+++ 4.3/million.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 14:15:53 2024
+
+@author: nardo
+"""
+
+for num in range(1,10000001):
+    print(num);
