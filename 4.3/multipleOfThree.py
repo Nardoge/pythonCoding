2058f5834951f262da7e02660f27bdeec15a6b45
huangjunwei <nardoge@163.com>
2024-04-19 15:57:48 +0800

    feat():4.6,4.7

---
+++ 4.3/multipleOfThree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 15:54:23 2024
+
+@author: nardo
+"""
+
+multipleOfThreeArr = range(3,31,3);
+
+
+for multipleOfThree in multipleOfThreeArr:
+    print(multipleOfThree);
---
+++ 4.3/oodNumbers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 15:47:52 2024
+
+@author: nardo
+"""
+
+oodNumbers = range(1,20,2);
+
+for oodNum in oodNumbers:
+    print(oodNum);
