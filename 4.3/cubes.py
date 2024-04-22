c97c47b228ca8edf5625c02dd422f457b6d44aee
huangjunwei <nardoge@163.com>
2024-04-22 08:46:41 +0800

    fix():先不用列表解析实现

--- 4.3/cubes.py
+++ 4.3/cubes.py
@@ -5,7 +5,10 @@ Created on Mon Apr 22 08:37:01 2024
 @author: nardo
 """
 
-cubes=[value**3 for value in range(1,11)]
+cubes = []
 
+for value in range(1,11):
+    cubes.append(value**3)
+    
 for cube in cubes:
     print(cube);
