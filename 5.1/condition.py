6f654ad717724b5f3d77afd8866302f78c0ae2c8
huangjunwei <nardoge@163.com>
2024-05-10 08:30:44 +0800

    feat():5.1

---
+++ 5.1/condition.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 08:19:11 2024
+
+@author: nardo
+"""
+
+car = 'subaru'
+print("Is car == 'subaru'?I predict True.")
+print(car == 'subaru')
+
+
+print('\n');
+
+print("Is car == 'audi'?I predict False.")
+print(car == 'audi');
+
+print('\n');
+
+cars =['audi','baoma','benz']
+
+print("Is car in bba? I predict False.")
+print (car in cars);
+
+print('\n');
+
+print("1 >  2 ? I predict False.")
+print (1 > 2);
+
+print('\n');
+print('1 + 1 = 2 ? I predict True.')
+print(1 + 1 == 2);
