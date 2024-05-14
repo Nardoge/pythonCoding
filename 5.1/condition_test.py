1944c43ebafb019c513a7ffd0f0b39856ddec5b6
huangjunwei <nardoge@163.com>
2024-05-14 08:34:46 +0800

    feat():5.2

---
+++ 5.1/condition_test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 08:21:13 2024
+
+@author: nardo
+"""
+
+print("abc" == "abc");
+
+print("abc" != "abc");
+
+print("C".lower() =="c")
+
+print(10 == 10)
+
+print (10 > 9)
+
+print(10 < 9)
+
+print(10 <= 9)
+
+print(10 >= 9)
+
+print((10 > 9) and (8 >7))
+
+print((10 > 9) or (7 >7))
+
+cars = ['audi','byd','qq']
+
+print('auto' in cars)
+
+print('auto' not in cars)
