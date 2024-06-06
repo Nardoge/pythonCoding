3a2f34b1476e9cb01800be44d33c44d26acbcf12
huangjunwei <nardoge@163.com>
2024-06-06 14:45:38 +0800

    feat():5.1

---
+++ 5.1/alien1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 08:31:47 2024
+
+@author: nardo
+"""
+
+alien_color = "green"
+
+if alien_color=="green":
+    print("get 5 point")
+    
+    
+alien_color = "red"
+if alien_color=="green":
+    print("get 5 point")
+
+    
