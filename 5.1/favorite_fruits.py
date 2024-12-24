d2154ced4bdb6732379ef9275813710676de4045
huangjunwei <nardoge@163.com>
2024-12-24 08:51:48 +0800

    fix():5.7

---
+++ 5.1/favorite_fruits.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 08:36:20 2024
+
+@author: nardo
+"""
+
+favorite_fruits = {"apple","orange","pear"}
+
+def is_your_favorite_fruits(fruit):
+    if fruit in favorite_fruits:
+        print("you really like " + fruit)
+
+fruit = "apple"
+is_your_favorite_fruits(fruit)
+
+
+fruit = "orange"
+is_your_favorite_fruits(fruit)
+
+fruit = "paie"
+is_your_favorite_fruits(fruit)
+
+fruit = "pear"
+is_your_favorite_fruits(fruit)
+
+fruit = "Mango"
+is_your_favorite_fruits(fruit)
+
+
+    
+    
+    
